dd75f0668245eb29b4e283d4c6e66f743d24ec21
kalish93 <kaleabtekalign@gmail.com>
2024-08-31 23:14:43 +0300

    feat: order status management

--- src/controllers/restaurant/orderController.js
+++ src/controllers/restaurant/orderController.js
@@ -1,102 +1,6 @@
 const { TableStatus, OrderStatus } = require('@prisma/client');
 const prisma = require('../../database');
-
-// async function createOrder(req, res) {
-//     try {
-//         const { restaurantId, tableId, items } = req.body;
-
-//         const table = await prisma.table.update({
-//             where:{id: tableId},
-//             data:{
-//                 status: TableStatus.OCCUPIED
-//             }
-//         })
-
-//         // Create a new order
-//         const order = await prisma.order.create({
-//             data: {
-//                 tableId: tableId,
-//                 restaurantId: restaurantId,
-//                 status: 'PENDING',
-//                 items: {
-//                     create: items.map(item => ({
-//                         quantity: parseInt(item.quantity),
-//                         specialInstructions: item.specialInstructions || '',
-//                         menuItem: {
-//                             connect: { id: item.menuItem.id }
-//                         }
-//                     }))
-//                 }
-//             },
-//             include: {
-//                 items: {
-//                     include: {
-//                         menuItem: true
-//                     }
-//                 }
-//             }
-//         });
-
-//         // Process each order item based on its destination
-//         for (const orderItem of order.items) {
-//             if (orderItem.menuItem.destination === 'KITCHEN') {
-//                 await prisma.kitchenOrder.create({
-//                     data: {
-//                         orderId: order.id,
-//                         status: 'PENDING'
-//                     }
-//                 });
-//             } else if (orderItem.menuItem.destination === 'BAR') {
-
-//                 if(orderItem.menuItem.stock){
-//                 const stock = await prisma.stock.findUnique({
-//                     where: { id: orderItem.menuItem.stockId }
-//                 });
-
-
-//                 if (stock.quantity < orderItem.quantity) {
-//                     return res.status(400).json({ error: `Insufficient stock for ${orderItem.menuItem.name}` });
-//                 }
-
-//                 await prisma.barOrder.create({
-//                     data: {
-//                         orderId: order.id,
-//                         stockId: orderItem.menuItem.stockId,
-//                         quantity: orderItem.quantity,
-//                         status: 'PENDING'
-//                     }
-//                 });
-
-//                 // Update stock quantity
-//                 await prisma.stock.update({
-//                     where: { id: orderItem.menuItem.stockId },
-//                     data: { quantity: stock.quantity - orderItem.quantity }
-//                 });
-//             }else{
-//                 await prisma.barOrder.create({
-//                     data: {
-//                         orderId: order.id,
-//                         quantity: orderItem.quantity,
-//                         status: 'PENDING'
-//                     }
-//                 });
-//             }
-//             }
-//         }
-
-//         return res.status(201).json(order);
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({ error: 'An error occurred while creating the order.' });
-//     }
-// }
-
-
-// const { TableStatus, OrderStatus } = require('@prisma/client');
-// const prisma = require('../../database');
 const io = require('../../../socketio');
-
-// console.log('IO instance:', io); // Add this line to check if io is imported correctly
 
 async function createOrder(req, res) {
     try {
@@ -133,15 +37,15 @@ async function createOrder(req, res) {
 
         // Create notifications for relevant users
         const kitchenUsers = await prisma.user.findMany({
-            where: { restaurantId, role: { name: 'Restaurant Manager' } } // Adjust role name as necessary
+            where: { restaurantId, role: { name: 'Kitchen Staff' } } // Adjust role name as necessary
         });
 
         const barUsers = await prisma.user.findMany({
-            where: { restaurantId, role: { name: 'Restaurant Manager' } } // Adjust role name as necessary
+            where: { restaurantId, role: { name: 'Bartender' } } // Adjust role name as necessary
         });
 
         const waiterUsers = await prisma.user.findMany({
-            where: { restaurantId, role: { name: 'Restaurant Manager' } } // Adjust role name as necessary
+            where: { restaurantId, role: { name: 'Waiter' } } // Adjust role name as necessary
         });
 
         // Notify kitchen staff based on order items
@@ -240,7 +144,247 @@ async function getActiveOrdersByTableId(req, res) {
     }
 }
 
+async function getActiveOrders(req, res) {
+    try {
+        const { role, restaurantId } = req.user;
+        // console.log(req.user);
+
+        let whereClause = {
+            restaurantId: restaurantId, // Ensure orders belong to the user's restaurant
+            status: {
+                in: [OrderStatus.PENDING, OrderStatus.IN_PROGRESS, OrderStatus.READY, OrderStatus.SERVED],
+            },
+        };
+
+        if (role.name === 'Kitchen Staff') {
+            whereClause.items = { some: { menuItem: { destination: 'KITCHEN' } } }; // Kitchen staff see only kitchen orders
+        } else if (role.name === 'Bartender') {
+            whereClause.items = { some: { menuItem: { destination: 'BAR' } } }; // Bar staff see only bar orders
+        } else if (role.name === 'Waiter' && role.name === 'Restaurant Manager') {
+            whereClause.status = { in: [OrderStatus.PENDING, OrderStatus.IN_PROGRESS, OrderStatus.READY, OrderStatus.SERVED, OrderStatus.PAYMENT_REQUESTED],}
+        }
+
+        const orders = await prisma.order.findMany({
+            where: whereClause,
+            include: {
+                items: {
+                    include: {
+                        menuItem: true,
+                    }
+                },
+                table: true,
+                kitchenOrders: true,
+                barOrders: true,
+            },
+            orderBy: {
+                createdAt: 'asc' // Orders are retrieved in the order they were created
+            }
+        });
+
+        // Compute role-specific status
+        const activeOrders = orders.map(order => {
+            const kitchenStatus = order.kitchenOrders.length > 0 
+                ? order.kitchenOrders[0].status 
+                : OrderStatus.PENDING;
+            const barStatus = order.barOrders.length > 0 
+                ? order.barOrders[0].status 
+                : OrderStatus.PENDING;
+                
+            const roleSpecificStatus = (role.name === 'Kitchen Staff')
+                ? kitchenStatus
+                : (role.name === 'Bartender')
+                ? barStatus
+                : order.status;
+                
+            return {
+                ...order,
+                roleSpecificStatus: roleSpecificStatus
+            };
+        });
+
+        return res.status(200).json(activeOrders);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'An error occurred while retrieving active orders.' });
+    }
+}
+
+
+async function getOrderHistory(req, res) {
+    try {
+        const { role, restaurantId } = req.user;
+
+        let whereClause = {
+            restaurantId: restaurantId, // Ensure orders belong to the user's restaurant
+            status: {
+                in: [OrderStatus.COMPLETED, OrderStatus.CANCELLED],
+            },
+        };
+
+        if (role === 'Kitchen Staff') {
+            whereClause.items = { some: { menuItem: { destination: 'KITCHEN' } } }; // Kitchen staff see only kitchen orders in history
+        } else if (role === 'Bartender') {
+            whereClause.items = { some: { menuItem: { destination: 'BAR' } } }; // Bar staff see only bar orders in history
+        } else if (role !== 'Waiter' && role !== 'Restaurant Manager') {
+            return res.status(403).json({ error: 'Unauthorized access' });
+        }
+
+        const orderHistory = await prisma.order.findMany({
+            where: whereClause,
+            include: {
+                items: {
+                    include: {
+                        menuItem: true,
+                    }
+                },
+                table: true,
+                kitchenOrders: true,
+                barOrders: true,
+            },
+            orderBy: {
+                createdAt: 'asc' // Orders are retrieved in the order they were created
+            }
+        });
+
+        return res.status(200).json(orderHistory);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'An error occurred while retrieving order history.' });
+    }
+}
+
+
+async function updateOrderStatus(req, res) {
+    const { id, newStatus, type } = req.body;
+    const { role } = req.user;
+  
+    const validStatuses = ['PENDING', 'IN_PROGRESS', 'READY', 'SERVED', 'CANCELLED'];
+  
+    if (!validStatuses.includes(newStatus)) {
+      return res.status(400).json({ error: 'Invalid status' });
+    }
+  
+    try {
+      let order;
+      let kitchenOrders = [];
+      let barOrders = [];
+  
+      // Helper function to determine the final order status
+      const determineFinalStatus = (kitchenOrders, barOrders) => {
+        const allKitchenReady = kitchenOrders.every(k => k.status === 'READY');
+        const allKitchenServed = kitchenOrders.every(k => k.status === 'SERVED');
+        const allBarReady = barOrders.every(b => b.status === 'READY');
+        const allBarServed = barOrders.every(b => b.status === 'SERVED');
+  
+        if (allKitchenServed && allBarServed) return 'SERVED';
+        if (allKitchenReady && allBarReady) return 'READY';
+        if (kitchenOrders.some(k => k.status === 'IN_PROGRESS') || barOrders.some(b => b.status === 'IN_PROGRESS')) return 'IN_PROGRESS';
+        return 'PENDING';
+      };
+
+      if (role.name === 'Kitchen Staff') {
+        order = await prisma.order.findUnique({ where: { id } });
+        if (!order) return res.status(404).json({ error: 'Order not found' });
+  
+        await prisma.kitchenOrder.updateMany({
+          where: { orderId: id },
+          data: { status: newStatus },
+        });
+
+
+        kitchenOrders = await prisma.kitchenOrder.findMany({
+            where: { orderId: id },
+          });
+    
+          barOrders = await prisma.barOrder.findMany({
+            where: { orderId: id },
+          });
+  
+  
+      } else if (role.name === 'Bartender') {
+        order = await prisma.order.findUnique({ where: { id } });
+        if (!order) return res.status(404).json({ error: 'Order not found' });
+  
+        await prisma.barOrder.updateMany({
+          where: { orderId: id },
+          data: { status: newStatus },
+        });
+  
+
+        kitchenOrders = await prisma.kitchenOrder.findMany({
+            where: { orderId: id },
+          });
+    
+          barOrders = await prisma.barOrder.findMany({
+            where: { orderId: id },
+          });
+  
+      } else if (role.name === 'Waiter' || role.name === 'Restaurant Manager') {
+        order = await prisma.order.findUnique({
+          where: { id },
+          include: {
+            kitchenOrders: true,
+            barOrders: true,
+          },
+        });
+        if (!order) return res.status(404).json({ error: 'Order not found' });
+
+        if(type === 'Kitchen'){
+            await prisma.kitchenOrder.updateMany({
+                where: { orderId: id },
+                data: { status: newStatus },
+              });
+        }else if(type === 'Bar'){
+            await prisma.barOrder.updateMany({
+                where: { orderId: id },
+                data: { status: newStatus },
+              });
+        }else{
+            await prisma.order.update({
+                where: { id },
+                data: { status: newStatus },
+              });
+
+             
+                await prisma.kitchenOrder.updateMany({
+                  where: { orderId: id },
+                  data: { status: newStatus },
+                });
+                await prisma.barOrder.updateMany({
+                  where: { orderId: id },
+                  data: { status: newStatus },
+                });
+        }
+  
+       
+      } else {
+        return res.status(403).json({ error: 'You do not have permission to update this order' });
+      }
+  
+      // Determine the final status based on sub-orders
+      const finalStatus = determineFinalStatus(kitchenOrders, barOrders);
+  
+     
+       if(kitchenOrders.length > 0 || barOrders.length > 0){
+        await prisma.order.update({
+            where: { id: id },
+            data: { status: finalStatus },
+          });
+       }
+      
+  
+      return res.status(200).json(order);
+    } catch (error) {
+      console.error('Error updating order status:', error.message);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+  
+
 module.exports = {
     createOrder,
-    getActiveOrdersByTableId
+    getActiveOrdersByTableId,
+    getActiveOrders,
+    getOrderHistory,
+    updateOrderStatus
 };
--- src/routes/restaurant/orderRoutes.js
+++ src/routes/restaurant/orderRoutes.js
@@ -7,4 +7,11 @@ const router = express.Router();
 router.get('/orders/active/:tableId',  orderController.getActiveOrdersByTableId);
 router.post('/orders',  orderController.createOrder);
 
+const authenticate = require('../../middlewares/authenticate');
+router.use(authenticate);
+
+router.put('/orders/status', orderController.updateOrderStatus);
+router.get('/orders/active', orderController.getActiveOrders);
+router.get('/orders/history', orderController.getOrderHistory);
+
 module.exports = router;
